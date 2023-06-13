d46d8e565c4455fc84bd03772ac8ef2a9d94991f
Wengel Lodebo <tes.wengel@gmail.com>
2023-06-13 04:25:52 +0000

    6

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -15,6 +15,14 @@ public class Fibonacci {
      * @return the nth number of fibonacci sequence.
      */
     public int fib(int n){
-        return 0;
+         int n1=0,n2=1;      
+         for (int i = 0; i < n; i ++)
+            {
+                int n3 = n1 + n2;
+                n1 = n2;
+                n2 = n3;
+            }
+        return n1;
     }
 }
+
